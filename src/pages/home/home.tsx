f2e23c9db8e0229e6f7a9f1cd8ceba1ac20cf457
haitham baklouti <haithem.baklouti@sesame.com.tn>
2022-05-29 17:28:57 +0200

    config redux and axios

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Home = () => {
+    return <h1>Your Card</h1>
+}
+export default Home;
---
+++ src/reducers/index.ts
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+
+export interface IRootState {
+
+};
+
+const rootReducer = combineReducers<IRootState>({
+
+});
+
+export default rootReducer;
